51383bb75e2d1bb4545684b280f627d7d5f867c6
rshorche <44744441+rshorche@users.noreply.github.com>
2024-05-16 11:20:39 +0330

    feat: about us create section 1

--- src/components/Path/Path.jsx
+++ src/components/Path/Path.jsx
@@ -12,22 +12,25 @@ export default function Path() {
   const activePage = Object.keys(pageNames).filter((page) => page === path)[0];
 
   return (
-    <div className="relative flex flex-col justify-center items-center mt-11 md:mt-28">
-      <img
-        src="src/assets/images/leaf.png"
-        alt=""
-        className="absolute left-0 md:left-1/4 top-0 "
-      />
-      <h2 className="text-4xl md:text-7xl"> {pageNames[activePage]}</h2>
-      <div className="my-10">
-        <span className="text-orange">Home / </span>
-        {path.slice(1)}
+    <>
+      <div className="relative flex flex-col justify-center items-center mt-11 md:mt-28">
+        <img
+          src="src/assets/images/leaf.png"
+          alt=""
+          className="absolute left-0 md:left-1/4 top-0 "
+        />
+        <h2 className="text-4xl md:text-7xl"> {pageNames[activePage]}</h2>
+        <div className="my-10">
+          <span className="text-orange">Home / </span>
+          {path.slice(1)}
+        </div>
+        <img
+          src="src/assets/images/leaf.png"
+          alt=""
+          className="absolute right-0 md:right-1/4 bottom-0 "
+        />
       </div>
-      <img
-        src="src/assets/images/leaf.png"
-        alt=""
-        className="absolute right-0 md:right-1/4 bottom-0 "
-      />
-    </div>
+      <hr className="w-full border-gray/10 mt-12 sm:mt-16 mb-20 sm:mb-24" />
+    </>
   );
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -4,6 +4,57 @@ export default function AboutUs() {
   return (
     <div className="px-4 sm:px-14">
       <Path />
+      {/* section 1 */}
+      <div className="flex flex-wrap lg:flex-nowrap justify-between gap-8 mt-28">
+        <div className="max-w-lg md:min-w-96">
+          <img src="src/assets/images/aboutUs1.png" alt="" className="w-full" />
+        </div>
+
+        <div className="max-w-lg">
+          <h3 className="text-4xl md:text-5xl">
+            به بهشت ما خوش آمدید شکوفا کردن و رشد کردن
+          </h3>
+          <p className="text-sm mt-6">
+            در Bloom & Grow، ما علاقه زیادی به گیاهان و آوردن زیبایی طبیعت به
+            خانه و محل کار مردم داریم. فروشگاه گیاهان ما 12 سال است که به جامعه
+            پونتیاناک خدمت می کند و ما به انتخاب گسترده گیاهان با کیفیت بالا و
+            تعهدمان به خدمات عالی به مشتریان افتخار می کنیم.
+          </p>
+          <p className="text-sm mt-6">
+            کارکنان آگاه ما به مشتریان خود کمک می کنند تا گیاه مناسب برای
+            نیازهای خود را پیدا کنند، چه یک ساکولنت کم نگهداری یا یک درخت
+            سرپوشیده سرپوشیده. ما همچنین طیف وسیعی از محصولات و لوازم جانبی
+            مراقبت از گیاه را ارائه می دهیم تا به مشتریان خود کمک کنیم تا گیاهان
+            خود را سالم و شکوفا نگه دارند.
+          </p>
+
+          <div className="flex flex-wrap gap-4 text-center *:max-w-28 *:bg-[#F5F5F5] *:rounded-xl *:p-2 *:*:m-1 mt-6">
+            <div>
+              <span>12+</span>
+              <p className="text-xs sm:text-sm">سال تجربه</p>
+            </div>
+            <div>
+              <span>35+</span>
+              <p className="text-xs sm:text-sm">فروشگاه </p>
+            </div>
+            <div>
+              <span>321k+</span>
+              <p className="text-xs sm:text-sm">مشتری راضی </p>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap gap-4 mt-8">
+            <button className="bg-orange text-white py-4 px-10 rounded-full text-sm md:text-base md:px-7">
+              اکنون خرید کنید
+            </button>
+            <button className="bg-white text-black py-4 px-10 rounded-full border text-sm md:text-base md:px-7">
+              خدمات ما
+            </button>
+          </div>
+        </div>
+      </div>
+
+   
     </div>
   );
 }
