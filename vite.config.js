cae05bd93f38a7a02028131ed0ccbe1ea3f7a3a1
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-05 22:42:33 +0330

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            "@ckeditor/ckeditor5-build-classic",
+            "@ckeditor/ckeditor5-react",
+            "@reduxjs/toolkit",
+            "@supabase/auth-ui-react",
+            "@supabase/auth-ui-shared",
+            "@supabase/supabase-js",
+            "react",
+            "react-dom",
+            "react-hook-form",
+            "react-icons",
+            "react-redux",
+            "react-router-dom",
+            "redux",
+            "swiper",
+          ],
+        },
+      },
+    },
+  },
+});
