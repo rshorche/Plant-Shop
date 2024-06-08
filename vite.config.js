cd51a223794189befad8fbf3f783b28b2caa9601
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-08 22:08:02 +0330

    Update   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import commonjs from "vite-plugin-commonjs";
-export default defineConfig(() => {
-    return {
-        plugins: [
-            react(),
-            commonjs({
-                filter(id) {
-                    if (["libs/ckeditor5/build/ckeditor.js"].includes(id)) {
-                        return true;
-                    }
-                },
-            }),
-        ],
-        optimizeDeps: {
-            include: ["ckeditor5-custom-build"],
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // تنظیمات نام فایل‌های دارایی‌ها (assets)
+        assetFileNames: assetInfo => {
+          // تعیین نوع فایل بر اساس پسوند
+          let extType = assetInfo.name.split('.').pop();
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
         },
-        build: {
-            commonjsOptions: { exclude: ["ckeditor5-custom-build"] },
-            outDir: 'build',
-            rollupOptions: {
-                output:{
-                    manualChunks(id) {
-                        if (id.includes('node_modules')) {
-                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                        }
-                    }
-                }
-            }
+        // تنظیمات نام فایل‌های chunk و entry
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        // تنظیمات جداسازی chunks
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0];
+          }
         },
-    };
+      },
+    },
+    // حد آستانه هشدار برای اندازه chunk
+    chunkSizeWarningLimit: 500,
+  },
 });
