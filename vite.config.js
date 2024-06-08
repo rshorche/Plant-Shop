044db3df5e0f51569152a8d8bf384ecd6b8a607b
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-08 18:52:28 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,33 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // 1️⃣ تنظیم برای نام‌گذاری فایل‌های استاتیک
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from "vite-plugin-commonjs";
+export default defineConfig(() => {
+    return {
+        plugins: [
+            react(),
+            commonjs({
+                filter(id) {
+                    if (["libs/ckeditor5/build/ckeditor.js"].includes(id)) {
+                        return true;
+                    }
+                },
+            }),
+        ],
+        optimizeDeps: {
+            include: ["ckeditor5-custom-build"],
         },
-        // 2️⃣ تنظیم برای نام‌گذاری فایل‌های chunk
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        // 3️⃣ تنظیم برای نام‌گذاری فایل‌های ورودی
-        entryFileNames: "assets/js/[name]-[hash].js",
-        // تنظیم برای manualChunks
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
+        build: {
+            commonjsOptions: { exclude: ["ckeditor5-custom-build"] },
+            outDir: 'build',
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    }
+                }
+            }
         },
-      },
-    },
-    // تنظیم برای محدودیت اندازه chunk
-    chunkSizeWarningLimit: 500,
-  },
+    };
 });
