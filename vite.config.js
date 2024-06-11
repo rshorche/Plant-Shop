535efa4e20387b076fd16951ffc42eed56aee8b3
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-11 18:02:33 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
-import { defineConfig } from 'vite'
-import vercel from '@vercel/plugin-vercel'
-
-export default defineConfig({
-  plugins: [
-    vercel(),
-  ],
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import commonjs from "vite-plugin-commonjs";
+export default defineConfig(() => {
+    return {
+        plugins: [
+            react(),
+            commonjs({
+                filter(id) {
+                    if (["libs/ckeditor5/build/ckeditor.js"].includes(id)) {
+                        return true;
+                    }
+                },
+            }),
+        ],
+        optimizeDeps: {
+            include: ["ckeditor5-custom-build"],
+        },
+        build: {
+            commonjsOptions: { exclude: ["ckeditor5-custom-build"] },
+            outDir: 'build',
+            rollupOptions: {
+                output:{
+                    manualChunks(id) {
+                        if (id.includes('node_modules')) {
+                            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        }
+                    }
+                }
+            }
+        },
+    };
 });
