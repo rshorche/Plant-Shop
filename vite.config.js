eb1b40a8568612b661036aed29010cb73ac2e225
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-11 18:00:34 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import vercel from '@vercel/plugin-vercel'
 
 export default defineConfig({
-  plugins: [react()],
-  },
+  plugins: [
+    vercel(),
+  ],
 });
