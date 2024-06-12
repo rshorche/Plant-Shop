5664d4d5bd052c61e83738558657ae7160b32a3e
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-12 08:37:53 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  builds: [
+    {
+      src: "package.json",
+      use: "@vercel/static-build",
+      config: {
+        distDir: "dist",
+      },
+    },
+  ],
+});
