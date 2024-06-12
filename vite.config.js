542e4b219d0e019c8e92579c85695286571d0794
rshorche <44744441+rshorche@users.noreply.github.com>
2024-06-12 08:39:42 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,31 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  builds: [
-    {
-      src: "package.json",
-      use: "@vercel/static-build",
-      config: {
-        distDir: "dist",
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
     },
-  ],
+    chunkSizeWarningLimit: 500,
+  },
 });
