48c338e36ac1a7e375d746290a219c077c0627cb
rshorche <44744441+rshorche@users.noreply.github.com>
2025-05-27 13:15:12 +0330

    Fix: Configure routes in vercel.json to correctly serve static assets

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,24 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        // assetFileNames: (assetInfo) => {
-        //   let extType = assetInfo.name.split(".").at(1);
-        //   if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-        //     extType = "img";
-        //   }
-        //   return `assets/${extType}/[name]-[hash][extname]`;
-        // },
-        // chunkFileNames: "assets/js/[name]-[hash].js",
-        // entryFileNames: "assets/js/[name]-[hash].js",
-        // manualChunks(id) {
-        //   if (id.includes("node_modules")) {
-        //     return id
-        //       .toString()
-        //       .split("node_modules/")[1]
-        //       .split("/")[0]
-        //       .toString();
-        //   }
-        // },
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split(".").at(1);
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "img";
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: "assets/js/[name]-[hash].js",
+        entryFileNames: "assets/js/[name]-[hash].js",
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
       },
     },
     chunkSizeWarningLimit: 500,
